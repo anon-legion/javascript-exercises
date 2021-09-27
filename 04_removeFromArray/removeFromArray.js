f907edc6ed64f85f76d175b4756f9037af416277
anon-legion <g.sync.mobile1@gmail.com>
2021-09-27 22:07:20 +0800

    finish removeFromArray.js TOP exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let result, toRemoveArray = [...arguments].slice(1, arguments.length);
+    console.log(toRemoveArray);
+    result = arr.filter(element => {
+        return toRemoveArray.every(target => target !== element)
+    });
+    return result;
 };
 
+// let test = [1,2,3,4], target = [2, 3];
+// console.log(removeFromArray(test, ...target));
 // Do not edit below this line
 module.exports = removeFromArray;
