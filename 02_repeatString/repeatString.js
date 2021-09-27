200a101bf2b1bf5814432d50285ca332a32ae5d0
anon-legion <g.sync.mobile1@gmail.com>
2021-09-27 14:21:29 +0800

    finish repeatString.js exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = '';
+    if (num < 0) {
+        result = 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            result += string;
+        }
+    };
+    return result;
 };
 
 // Do not edit below this line
