704f341d4af8f5273a3c4e7f015453e30cbbd52d
anon-legion <g.sync.mobile1@gmail.com>
2021-09-28 13:10:14 +0800

    finish palindromes.js TOP exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let letterArr = str.toLowerCase().split('').filter(element => /[a-z]/.test(element));
+    const isPalindrome = (letters) => {
+        if (letters.length <= 1) return true;
+        else {
+            return letters[0] === letters[letters.length - 1] && isPalindrome(letters.slice(1, -1));
+        }
+    };
+    return isPalindrome(letterArr);
 };
 
 // Do not edit below this line
