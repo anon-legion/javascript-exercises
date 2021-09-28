fc84e4a78af28fc2e4f0790ec9240b8a96ed9ba4
anon-legion <g.sync.mobile1@gmail.com>
2021-09-28 10:40:41 +0800

    finish sumAll.js TOP exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+// const sumAll = function(num1, num2) {
+//     let sum = 0;
+//     for (num1; num1 < num2 + 1; num1++) {
+//         sum += num1;
+//     }
+//     return sum;
+// };
 
-};
+const sumAll = function(num1, num2) {
+    if(Math.min(...arguments) < 0 || typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+        return 'ERROR';
+    } else {
+        let numElements = Math.abs(num1 - num2) + 1;
+        return (num1 + num2) * (numElements / 2);
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
