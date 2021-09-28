246139f878273a01f6c1747f093dffad3f55984c
anon-legion <g.sync.mobile1@gmail.com>
2021-09-28 11:05:23 +0800

    finish leapYear.js TOP exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (year % 4 === 0) {
+            return true;
+        } else {
+            return false;
+        };
+    };
 };
 
 // Do not edit below this line
