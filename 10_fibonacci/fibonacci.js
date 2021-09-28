6ba9a3097d3ac18c0819ceaf3af093fd03d4fbed
anon-legion <g.sync.mobile1@gmail.com>
2021-09-28 14:21:15 +0800

    finish fibonacci.js TOP exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
 
+const fibonacci = function(n) {
+    const recursiveFibonacci = (num, a=0, b=1) => {
+        if (num === 0) return a;
+        if (num === 1) return b;
+        else {
+            return recursiveFibonacci(num - 1, b, a + b);
+        }
+    };
+    let normalizedNum = parseInt(n);
+    if (normalizedNum < 0) return 'OOPS';
+    else {
+        return recursiveFibonacci(normalizedNum);
+    }
 };
 
 // Do not edit below this line
