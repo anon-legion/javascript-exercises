0c66b8e4382b86876152f6711003317493c4c2b6
anon-legion <g.sync.mobile1@gmail.com>
2021-09-28 11:44:32 +0800

    finish tempConversion.js TOP exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) * 5/9;
+  return Math.round((cTemp + Number.EPSILON) * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = (cTemp * 9/5) + 32;
+  return Math.round((fTemp + Number.EPSILON) * 10) / 10
 };
 
 // Do not edit below this line
